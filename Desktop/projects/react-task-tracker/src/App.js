fcaafc1a71f96637d7bc6b88cfe7f030b11b84dd
celestialheathen <jingchaoyu@hotmail.com>
2021-06-26 17:50:34 -0400

    basic app

--- Desktop/projects/react-task-tracker/src/App.js
+++ Desktop/projects/react-task-tracker/src/App.js
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div className="container">
-      <Header title="Hell NOOO"/>
+      <Header title="Task Tracker"/>
     </div>
   );
 }
--- Desktop/projects/react-task-tracker/src/components/Header.js
+++ Desktop/projects/react-task-tracker/src/components/Header.js
@@ -1,9 +1,24 @@
-const Header = (props) => {
+import PropTypes from 'prop-types'
+
+const Header = ({title}) => {
   return (
-    <header>
-      <h1>{props.title}</h1>
+    <header className='header'>
+      <h1>{title}</h1>
+      <button className='btn'>Add</button>
     </header>
   )
 }
+
+Header.defaultProps = {
+  title: 'My Task Tracker 1.1'
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+// const headingStyle = {
+//   color: 'blue', backgroundColor: 'black'
+// }
 
 export default Header
