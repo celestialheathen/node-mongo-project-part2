a5cd86a3bffe0693eb17ac7ad90a5cad45519752
celestialheathen <jingchaoyu@hotmail.com>
2021-06-29 16:07:53 -0400

    button color

--- Desktop/projects/react-task-tracker/src/App.js
+++ Desktop/projects/react-task-tracker/src/App.js
@@ -5,6 +5,8 @@ import AddTask from './components/AddTask'
 
 function App() {
 
+  const [showAddTask, setShowAddTask] = useState(true)
+
   const [tasks, setTasks] = useState([
     {
       "id": 1,
@@ -48,8 +50,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={ () => setShowAddTask(!showAddTask)} showAddTask={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : ('No Tasks To Show')}
     </div>
   );
