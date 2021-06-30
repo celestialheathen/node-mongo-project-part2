2d1fda87fa27aa17489bc443776b2eb132cae460
celestialheathen <jingchaoyu@hotmail.com>
2021-06-30 15:23:53 -0400

    async

--- Desktop/projects/async_javascript/exercise.js
+++ Desktop/projects/async_javascript/exercise.js
@@ -1,11 +1,22 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
-.then(res => res.json())
-.then(data => console.log(data.length))
+// fetch('https://jsonplaceholder.typicode.com/posts')
+// .then(res => res.json())
+// .then(data => console.log(data.length))
 
-async function getPosts() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-  const data = await res.json()
-  console.log(data)
+// Get
+// async function getPosts() {
+//   const res = await fetch(`http://localhost:3000/tasks`)
+//   const data = await res.json()
+//   console.log(data)
+// }
+// getPosts()
+
+// Delete
+// async function deletePost(id) {
+//   const res = await fetch(`http://localhost:3000/tasks/${id}`, {method: "DELETE"})
+// }
+// deletePost(2)
+
+// PATCH
+async function addPost() {
+  
 }
-
-getPosts()
