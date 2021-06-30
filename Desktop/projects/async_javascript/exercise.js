1cc1958be37c3f021a072337608a7d132f9c8163
celestialheathen <jingchaoyu@hotmail.com>
2021-06-30 15:33:23 -0400

    exercises

--- Desktop/projects/async_javascript/exercise.js
+++ Desktop/projects/async_javascript/exercise.js
@@ -16,7 +16,30 @@
 // }
 // deletePost(2)
 
+// POST
+// async function addPost(newTask) {
+//   const res = await fetch(`http://localhost:3000/tasks`, {
+//     method: "POST", 
+//     headers: {'Content-Type': 'application/json'},
+//     body: JSON.stringify(newTask)
+//   })
+//   // const data = await res.json()
+//   // console.log(data)
+// }
+// addPost({ id: 2,
+//           text: "Dating in the summer",
+//           day: "Summer....",
+//           reminder: true})
+
 // PATCH
-async function addPost() {
-  
-}
+// async function updatePost(id, info) {
+//     const res = await fetch(`http://localhost:3000/tasks/${id}`, {
+//     method: "PATCH", 
+//     headers: {'Content-Type': 'application/json'},
+//     body: JSON.stringify(info)
+//   })
+//   const data = await res.json()
+//   console.log(data)
+// }
+
+// updatePost(2, {day: "This scorching summer in NYC..."})
